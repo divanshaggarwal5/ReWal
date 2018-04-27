dd2f519f3555e7d734705b41dbee290a3df38308
Divanshu Garg <divansh.aggarwal5@gmail.com>
2018-04-27 09:58:04 -0700

    Add project files.

---
+++ ReWal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReWal.Startup))]
+namespace ReWal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
